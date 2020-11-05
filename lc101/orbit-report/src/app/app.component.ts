5fa4a13b2e177638f6d80d12b4e5651f3e49c8ec
Anya Wynne <avkwynne@gmail.com>
2020-11-05 13:17:11 -0600

    finished part 1-5

--- lc101/orbit-report/src/app/app.component.ts
+++ lc101/orbit-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Satellite} from './satellite'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    let satelliteURL = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satelliteURL).then(function(response){
+      response.json().then(function(data){
+        let fetchedSatellites = data.satellites;
+        
+        for (let i=0; i < fetchedSatellites.length; i++){
+          let jsonSatellite= new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          this.sourceList.push(jsonSatellite);
+        }
+      }.bind(this));
+    }.bind(this));
+
+  }
 }
