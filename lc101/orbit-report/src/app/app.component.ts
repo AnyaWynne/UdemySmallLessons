c591c1a444321fb3ca9627ba96907fb2f99c9424
Anya Wynne <avkwynne@gmail.com>
2020-11-06 17:24:17 -0600

    finished parts 1-8

--- lc101/orbit-report/src/app/app.component.ts
+++ lc101/orbit-report/src/app/app.component.ts
@@ -9,9 +9,12 @@ import {Satellite} from './satellite'
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
+    
     let satelliteURL = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satelliteURL).then(function(response){
@@ -22,8 +25,23 @@ export class AppComponent {
           let jsonSatellite= new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           this.sourceList.push(jsonSatellite);
         }
+        this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));
 
   }
+
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    this.displayList = matchingSatellites;
+ }
+
 }
